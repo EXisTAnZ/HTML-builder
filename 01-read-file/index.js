ef007e749ccd859957d7ec28fb0d5855b7ba9bea
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2022-11-01 17:39:43 +0300

    feat: add solution to 1 task read-file 	modified:   01-read-file/index.js

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const filename = path.join(__dirname, 'text.txt');
+const stream = new fs.ReadStream(filename);
+
+stream.on('readable', function(){
+  const data = stream.read();
+  if(!!data) console.log(data.toString());
+})
