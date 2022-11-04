f4a27bdb689a3a9bf0bc1fd09114b98200772597
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2022-11-04 16:12:47 +0300

    feat: add solution to 04 task copy directory 	modified:   04-copy-directory/index.js

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const fromFolder = path.join(__dirname, 'files');
+const toFolder = path.join(__dirname, 'files-copy');
 
+fs.promises.rm(toFolder, { recursive: true, force: true })
+  .then(() => {
+    fs.promises.mkdir(toFolder, { recursive: true })
+      .then(() => copyDir());
+      
+  });
+
+function copyDir() {
+  fs.promises.readdir(fromFolder)
+    .then((files) => {
+      files.forEach(file => {
+        fs.promises.copyFile(`${fromFolder}/${file}`, `${toFolder}/${file}`);
+      });
+    });
+}
