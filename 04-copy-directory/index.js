6d591debadff39dce3ff30216c49464424f1f37a
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2022-11-05 14:54:49 +0300

    refactor: remove function to simple insert in 6 task 	modified:   04-copy-directory/index.js

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,17 +6,14 @@ const toFolder = path.join(__dirname, 'files-copy');
 fs.promises.rm(toFolder, { recursive: true, force: true })
   .then(() => {
     fs.promises.mkdir(toFolder, { recursive: true })
-      .then(() => copyDir());
-      
-  });
-
-function copyDir() {
-  fs.promises.readdir(fromFolder)
-    .then((files) => {
-      files.forEach(file => {
-        fs.promises.copyFile(
-          path.join(fromFolder, file), 
-          path.join(toFolder, file));
+      .then(() => {
+        fs.promises.readdir(fromFolder)
+          .then((files) => {
+            files.forEach(file => {
+              fs.promises.copyFile(
+                path.join(fromFolder, file),
+                path.join(toFolder, file));
+            });
+          });
       });
-    });
-}
+  });
